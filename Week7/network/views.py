86be4502424c743049ddc70097a0576bc6aa6a88
Brandon Fazal <jbtfazal@gmail.com>
2023-01-16 14:31:03 -0600

    started models, front end

--- Week7/network/views.py
+++ Week7/network/views.py
@@ -1,14 +1,18 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from .models import User, Post
 
 
 def index(request):
-    return render(request, "network/index.html")
+    posts = Post.objects.all()
+    return render(request, "network/index.html", {
+        "posts" : posts
+    })
 
 
 def login_view(request):
