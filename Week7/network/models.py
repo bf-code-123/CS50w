005310b838c0ad50c305ddd10fcfc282e013b17e
Brandon Fazal <jbtfazal@gmail.com>
2023-01-16 14:13:09 -0600

    started models, front end

--- Week7/network/models.py
+++ Week7/network/models.py
@@ -4,3 +4,9 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Post(models.Model):
+    content = models.CharField(max_length=256)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, blank=False, related_name="posts", default="")
+    #related name allows to search for all posts for a given user
